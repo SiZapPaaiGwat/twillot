a9353b916e31d9c4caf5552675e241eef1c13dc8
SiZapPaaiGwat <hello@twillot.com>
2024-05-28 22:02:02 +0800

    Auto Comment works

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,39 @@
 import { authTwitter, openOptionsPageWhenIconClicked } from '../libs/browser'
-import initWorkflows from '../libs/workflow/bg'
+import { TriggerMonitor } from '../libs/workflow/trigger'
+import {
+  Action,
+  Message,
+  MessageType,
+  TriggerReponsePayload,
+  Workflow,
+} from '../libs/workflow/types'
+import actions from '../libs/workflow/actions'
 
 openOptionsPageWhenIconClicked()
 authTwitter()
-// TODO wait until authentication is complete
-initWorkflows()
 
-console.log('background script loaded')
+function initWorkflows() {
+  const monitor = new TriggerMonitor()
+  for (const [key, value] of Object.entries(actions)) {
+    monitor.register(key as Action, value)
+  }
+
+  chrome.runtime.onMessage.addListener((message: Message) => {
+    if (message.type === MessageType.GetTriggerResponse) {
+      monitor.emit(message.payload as TriggerReponsePayload)
+    } else if (message.type === MessageType.GetWorkflows) {
+      const workflows = message.payload as Workflow[]
+      if (!workflows || workflows.length === 0) {
+        return
+      }
+
+      chrome.storage.local.set({ workflows })
+      monitor.workflows = workflows
+    } else {
+      console.log('Unknown message type:', message)
+    }
+  })
+  monitor.init()
+}
+
+initWorkflows()
